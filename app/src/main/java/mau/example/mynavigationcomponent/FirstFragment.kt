c384bc3a911b68b0442b3a94eb21539895934bb6
Mauricio <stb539@gmail.com>
2024-05-22 11:15:07 -0400

    fragments

--- app/src/main/java/mau/example/mynavigationcomponent/FirstFragment.kt
+++ app/src/main/java/mau/example/mynavigationcomponent/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class FirstFragment : Fragment() {
 
@@ -12,7 +14,14 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_first, container, false)
+        // en vez de usar viewBinding, se puede usar findViewById
+        val btnNavigate = rootView.findViewById<Button>(R.id.btnNavigate)
+        btnNavigate.setOnClickListener {
+            // buscar el controlador de navegacion
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+        // retornar la vista inflada the layout for this fragment
+        return rootView
     }
 }
--- app/src/main/java/mau/example/mynavigationcomponent/SecondFragment.kt
+++ app/src/main/java/mau/example/mynavigationcomponent/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class SecondFragment : Fragment() {
 
@@ -12,7 +14,14 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_second, container, false)
+        // en vez de usar viewBinding, se puede usar findViewById
+        val btn2 = rootView.findViewById<Button>(R.id.btn2)
+        btn2.setOnClickListener {
+            // buscar el controlador de navegacion
+            findNavController().navigate(R.id.action_secondFragment_to_thirdFragment)
+        }
+        // retornar la vista inflada the layout for this fragment
+        return rootView
     }
 }
